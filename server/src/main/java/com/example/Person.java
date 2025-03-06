55a4d78804a83e2e4ca343d2d22f16fbe0a13e56
shirleyhuang <shirleyhuamg@johnsonfitness.com>
2025-03-06 16:00:30 +0800

    Fix checkstyle warning.

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -12,8 +12,11 @@ public class Person {
 
   }
 
-  //TODO: Add javadoc comment
-  public String person(String someone) {
+  /**
+   * @param someone 是人名
+   * @return 字串符
+   */
+  public final String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,5 @@
+package com.example;
+
+/**
+ * This is a package name.
+ */
